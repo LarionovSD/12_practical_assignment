27ed16cd98dcf7392e4ee4f6eb7ee51dd98fd055
Sergey <itog24>
2025-12-25 18:20:41 +0500

    add calculate_tax and tests

--- src/main.py
+++ src/main.py
@@ -13,3 +13,13 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         taxed_prices.append(price + tax)
 
     return taxed_prices
+
+
+def calculate_tax(price: float, tax_rate: float) -> float:
+    """Функция вычисляет стоимость товара с учетом налога"""
+
+    if price <= 0:
+        raise ValueError('Неверная цена')
+    if tax_rate < 0 or tax_rate >= 100:
+        raise ValueError('Неверный налоговый процент')
+    return round(price * (1 + tax_rate / 100), 2)
